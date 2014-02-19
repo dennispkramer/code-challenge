9d6c39e7f1be52aae4a1e31082773c5f492b3bfe
Dennis Kramer <dennispkramer@gmail.com>
2014-02-19 17:19:16 -0500

    first commit

---
+++ source/javascripts/all.js
@@ -0,0 +1,11 @@
+//= require jquery
+$(document).ready(function(){
+  $("#preview_2").click(function(){
+    $(".modal-tarp").toggleClass("hide animated fadeIn",function(){
+      $(this).remove();
+    });
+    $("#preview_2").toggleClass("slide_out", function(){
+      $(this).remove();
+    });
+  });
+});
